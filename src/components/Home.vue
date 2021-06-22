0fb4a87e053841a402809e0a3b062c77787a213c
layfredrc <47161258+layfredrc@users.noreply.github.com>
2021-06-22 16:03:53 +0200

    connexion UI + Home bug fixes + url added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,19 @@
        <div>
             <h1>Votez est une plateforme sécurisée pour effectuer votre <mark>vote électorale</mark></h1>
             <p>Notre application est la solution proposée par l’État pour sécuriser et simplifier le vote électoral en ligne.</p>
-            <div>
-                <img src="./images/fusee.png" alt="fusée">
-                <p>Gagnez du temps</p>
-            </div>
-            <div>
-                <img src="./images/bouclier.png" alt="fusée">
-                <p>Sécurisez vos informations</p>
-            </div>
-            <div>
-                <img src="./images/compte.png" alt="fusée">
-                <p>Ne créez plus de nouveau compte</p>
+            <div class="icon-container">
+                <div>
+                    <img src="./images/fusee.png" alt="fusée">
+                    <p>Gagnez du temps</p>
+                </div>
+                <div>
+                    <img src="./images/bouclier.png" alt="fusée">
+                    <p>Sécurisez vos informations</p>
+                </div>
+                <div>
+                    <img src="./images/compte.png" alt="fusée">
+                    <p>Ne créez plus de nouveau compte</p>
+                </div>
             </div>
        </div>
     </div>
@@ -39,8 +41,14 @@ module.exports = {
 </script>
 
 <style scoped>
+
 .container{
-    background-color: #f7f7f7;
-    height: 20vh;
+    padding: 40px 235px;
 }
+
+.icon-container{
+    display: flex;
+    justify-content: space-between;
+}
+
 </style>
