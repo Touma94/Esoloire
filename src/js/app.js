363f06c197f77e038311a50c01f38893953a79a6
Grandkhan <kurt.cobain@tutanota.com>
2021-07-05 10:18:38 +0200

    app.js connection fonctionne, getcandidats et getresultats

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,101 @@
+const { networks } = require("../../truffle-config");
+const { RelayProvider } = require('@opengsn/provider')
+const Web3 = require("web3")
+ethers = require("ethers")
+
 App = {
   web3Provider: null,
-  contracts: {},
-  account: '0xBED9646559c43896edD205Ab7Ee85201e45d038a',
-  web3: null,
+  election: null,
+  account: "",
 
   init: async function () {
     return await App.initWeb3();
   },
 
   initWeb3: async function () {
-    const { RelayProvider } = require('@opengsn/provider')
-    const config = { 
-      paymasterAddress
+    const conf = {
+      ourContract: '0x40376aa2891436dd7EBc305b2041939ECd98d515',
+      paymaster: '0x9e59f087129F92ddDe8Ce90c204fec6423897199',
+      forwarder: '0x83A54884bE4657706785D7309cf46B58FE5f6e8a',
+      gasPrice: 16000000000   // 16 Gwei
     }
-    App.web3Provider = await RelayProvider.newProvider({ provider: new Web3.providers.HttpProvider('http://localhost:7545'), config }).init()
-    // if (window.ethereum) {
-    //   App.web3Provider = window.ethereum;
-    //   try {
-    //     await window.ethereum.enable();
-    //   } catch (error) {
-    //     console.error("User denied account access")
-    //   }
-    // }
-    // else if (window.web3) {
-    //   App.web3Provider = window.web3.currentProvider;
-    // }
-    // else {
-      // App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      console.log('web3 initialise')
-    // }
-    App.web3 = new Web3(App.web3Provider);
+    const config = {
+      paymasterAddress: '0x9e59f087129F92ddDe8Ce90c204fec6423897199'
+    }
 
+    let origProvider = networks.rinkerby.provider();
+
+    App.web3Provider = await RelayProvider.newProvider({ provider: origProvider, config }).init()
+    App.account = origProvider.addresses[0];
     return App.initContract();
   },
 
   initContract: function () {
+    
     $.getJSON("Election.json", function (election) {
+      // App.election = new web3.eth.Contract(
+      //   election.abi,
+      //   election.networks[4].address
+      // );
+      // console.log(App.election.methods)
       // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election);
+      App.election = TruffleContract(election);
       // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider);
-
-      // App.listenForEvents();
+      App.election.setProvider(App.web3Provider);
+      if (typeof  App.election.currentProvider.sendAsync !== "function") {
+        App.election.currentProvider.sendAsync = function() {
+          return App.election.currentProvider.send.apply(
+            App.election.currentProvider,
+                arguments
+          );
+        };
+      }
       App.getInfos();
     });
   },
 
   getInfos: async function () {
-      let instance = await App.contracts.Election.deployed();
-      let cpt = await instance.nbrCandidats();
-      let candidats = [];
-      for (let i=1; i <= cpt; i++){
-        let cd = await instance.candidats(i);
-        candidats[i-1] = cd[1];
-      }
-      // App.getResultats();
-      return candidats;
+    let instance = await App.election.deployed();
+    let cpt = await instance.nbrCandidats();
+    // let cpt = await App.election.methods.nbrCandidats().call();
+
+    let candidats = [];
+    for (let i = 1; i <= cpt; i++) {
+      let cd = await instance.candidats(i);
+      candidats[i - 1] = cd[1];
+    }
+    App.voter(2);
+    return candidats;
   },
 
   getResultats: async function () {
-    let instance = await App.contracts.Election.deployed();
+    let instance = await App.election.deployed();
     let total = await instance.totalVotes();
     let cpt = await instance.nbrCandidats();
-    let resultat = {nom:['total'], nbrVotes:[total.toNumber()]};
-    for (let i=1; i <= cpt; i++){
+    let resultat = { nom: ['total'], nbrVotes: [total] };
+    for (let i = 1; i <= cpt; i++) {
       let cd = await instance.candidats(i);
       resultat.nom[i] = cd[1]
-      resultat.nbrVotes[i] = cd[2].toNumber();
+      resultat.nbrVotes[i] = cd[2];
     }
     return resultat;
+  },
+
+  voter: async function (bulletinNum) {
+    let instance = await App.election.deployed();
+    provider = new ethers.providers.Web3Provider(App.web3Provider);
+    acct = provider.provider.newAccount();
+    signer = provider.getSigner(acct.address, acct.privateKey);
+    contract = await new ethers.Contract(instance.address, instance.abi, signer)
+    let receipt1 = await instance.donnerDroitDeVote(acct.address, { from: App.account });
+    // transaction = await contract.voter(bulletinNum)
+    // receipt = await provider.waitForTransaction(transaction.hash)
+    // console.log(receipt)
+    // let cit = await instanc.citoyens(from);
+    // console.log(cit);
+    // let receipt = await App.election.methods.voter(bulletinNum).send({ from });
+    // console.log(`Transaction hash: ${receipt.transactionHash}`);
+
   },
 
 };
